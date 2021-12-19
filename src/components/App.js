637634f1c7956c9b04786f12c291dd33b677eb29
Jeremy Blalock <jeremyjblalock@gmail.com>
2021-12-18 20:43:19 -0600

    CSS-based

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Arm from './Arm'
+import './App.css'
+
+function App() {
+  return (
+    <div className="app">
+      <Arm />
+    </div>
+  )
+}
+
+export default App
