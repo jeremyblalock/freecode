c5387b37a792b5e6d68f3bfdd929bcbdecdd76b8
Jeremy Blalock <jeremyjblalock@gmail.com>
2021-12-19 17:59:18 -0600

    Adds basic physics

--- src/index.js
+++ src/index.js
@@ -1,69 +1,168 @@
 import * as Three from 'three'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls'
+import { PhysicsLoader } from 'enable3d'
+import { AmmoPhysics } from '@enable3d/ammo-physics'
 import './styles.css'
 
-const scene = new Three.Scene()
+const setup = () => {
+  const scene = new Three.Scene()
 
-const camera = new Three.PerspectiveCamera(
-  75,
-  window.innerWidth / window.innerHeight,
-  0.1,
-  1000
-)
+  const camera = new Three.PerspectiveCamera(
+    75,
+    window.innerWidth / window.innerHeight,
+    0.1,
+    1000
+  )
 
-const renderer = new Three.WebGLRenderer()
+  const orthoCamera = new Three.OrthographicCamera(
+    -window.innerWidth / 2,
+    window.innerWidth / 2,
+    window.innerHeight / 2,
+    -window.innerHeight / 2,
+    -10000,
+    10000
+  )
 
-const setSize = () => {
-  const width = window.innerWidth
-  const height = window.innerHeight
+  // orthoCamera.matrixAutoUpdate = false
 
-  renderer.setSize(width, height)
+  const renderer = new Three.WebGLRenderer({ antialias: true })
+  renderer.setPixelRatio(window.devicePixelRatio)
+  renderer.shadowMap.enabled = true
 
-  // camera.left = width / 2
-  // camera.right = width / 2
-  // camera.top = height / 2
-  // camera.bottom = -height / 2
+  const setSize = () => {
+    const width = window.innerWidth
+    const height = window.innerHeight
+
+    renderer.setSize(width, height)
+
+    // Update camera
+    camera.aspect = window.innerWidth / window.innerHeight
+    camera.updateProjectionMatrix()
+
+    // Update orthographic camera
+    orthoCamera.left = -window.innerWidth
+    orthoCamera.right = window.innerWidth
+    orthoCamera.top = window.innerHeight
+    orthoCamera.bottom = -window.innerHeight
+    orthoCamera.updateProjectionMatrix()
+  }
+
+  window.addEventListener('resize', setSize)
+
+  setSize()
+
+  const el = document.getElementById('root')
+  el.appendChild(renderer.domElement)
+
+  // Setup Geometry
+  const geometry = new Three.BoxGeometry(100, 100, 100)
+
+  const material = new Three.MeshLambertMaterial({
+    color: '#fff',
+    side: Three.DoubleSide,
+  })
+
+  const cube = new Three.Mesh(geometry, material)
+  cube.castShadow = true
+  cube.rotation.y = Math.PI / 4
+  cube.position.y = 150
+
+  scene.add(cube)
+
+  // Add Line
+  const lineMaterial = new Three.LineBasicMaterial({ color: '#fff' })
+
+  const points = [
+    new Three.Vector3(-200, 0, 0),
+    new Three.Vector3(0, 200, 0),
+    new Three.Vector3(200, 0, 0),
+  ]
+
+  const lineGeo = new Three.BufferGeometry().setFromPoints(points)
+
+  const line = new Three.Line(lineGeo, lineMaterial)
+  line.position.y = 150
+  scene.add(line)
+
+  // Add plane
+  const planeGeo = new Three.PlaneGeometry(10000, 10000)
+
+  const planeMaterial = new Three.MeshLambertMaterial({
+    color: '#ccc',
+    side: Three.DoubleSide,
+  })
+
+  const plane = new Three.Mesh(planeGeo, planeMaterial)
+  plane.rotation.x = Math.PI / 2
+  plane.receiveShadow = true
+
+  scene.add(plane)
+
+  // Add light
+  const light = new Three.PointLight('#7df', 1, 100000)
+  scene.add(light)
+
+  light.position.set(200, 500, 600)
+  light.castShadow = true
+
+  // Light Shadow
+  light.shadow.mapSize.width = 512
+  light.shadow.mapSize.height = 512
+
+  const ambientLight = new Three.AmbientLight('#865')
+  scene.add(ambientLight)
+
+  // Set camera position
+  camera.position.z = 500
+  orthoCamera.position.z = 500
+  orthoCamera.position.y = window.innerHeight / 2
+  orthoCamera.rotation.x = -Math.PI / 6
+
+  // const baseRotation = new Three.Matrix4().makeRotationX(-Math.PI / 6)
+
+  // const baseMatrix = baseRotation.multiply(
+  //   new Three.Matrix4().makeTranslation(0, 50, 300)
+  // )
+
+  const physics = new AmmoPhysics(scene)
+  physics.debug.enable(true)
+  physics.add.ground({ width: 1000, height: 1000 })
+
+  physics.add.existing(cube)
+  cube.body.setCollisionFlags(0)
+  cube.body.bounciness = 0.7
+
+  const controls = new OrbitControls(orthoCamera, renderer.domElement)
+  controls.update()
+
+  let rotation = 0
+  const clock = new Three.Clock()
+
+  const loop = () => {
+    // Animate
+    rotation += 0.01
+    cube.rotation.y = rotation
+    cube.body.needUpdate = true
+
+    // const rotationMatrix = new Three.Matrix4().makeRotationY(rotation)
+    // const matrix = rotationMatrix.multiply(baseMatrix)
+
+    // orthoCamera.matrixWorld = matrix
+    // camera.updateMatrixWorld(true)
+
+    // Update physics
+    physics.update(clock.getDelta() * 1000)
+    physics.updateDebugger()
+
+    // Re-render
+    renderer.render(scene, orthoCamera)
+    requestAnimationFrame(loop)
+  }
+
+  loop()
 }
 
-window.addEventListener('resize', setSize)
-
-setSize()
-
-const el = document.getElementById('root')
-el.appendChild(renderer.domElement)
-
-// Setup Geometry
-const geometry = new Three.BoxGeometry(1, 1, 1)
-
-const material = new Three.MeshLambertMaterial({
-  color: '#fff',
-  side: Three.DoubleSide,
+PhysicsLoader('/static/js', (...args) => {
+  console.log('GOT ARGS:', args)
+  setup()
 })
-
-const cube = new Three.Mesh(geometry, material)
-scene.add(cube)
-
-// Add light
-const light = new Three.PointLight(0xffffff, 1, 100000)
-light.position.set(50, 50, 50)
-scene.add(light)
-
-const ambientLight = new Three.AmbientLight(0x404040)
-scene.add(ambientLight)
-
-// Set camera position
-camera.position.z = 5
-
-let rotation = 0
-
-const loop = () => {
-  // Animate
-  rotation += 1
-  //cube.rotation.x = 0.01 * rotation
-  cube.rotation.y = 0.01 * rotation
-
-  // Ren-render
-  renderer.render(scene, camera)
-  requestAnimationFrame(loop)
-}
-
-loop()
